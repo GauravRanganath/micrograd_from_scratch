418e6c982f9e1640cddd535807345aec32984602
gauravranganath <gaurav.ranganath@gmail.com>
2024-02-24 20:27:57 -0800

    add value class

---
+++ micrograd.py
@@ -0,0 +1,14 @@
+class Value:
+    def __init__(self, data, _children=(), _op=''):
+        self.data = data
+        self._prev = set(_children)
+        self._op = _op
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
+
+    def __add__(self, other):
+        return Value(self.data + other.data, (self, other), '+')
+
+    def __mul__(self, other):
+        return Value(self.data * other.data, (self, other), '*')
